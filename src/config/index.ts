856d795a0c0dd4be5cb6948d44c4c30b71b0056d
codedump00 <blazinasis@gmail.com>
2020-08-26 13:35:37 +0545

    reverted to secure channel

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ import { createTransport } from 'nodemailer';
 
 export const transporter = createTransport({
     host: process.env.MAIL__HOST,
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.MAIL__MAIL,
         pass: process.env.MAIL__PASSWORD,
