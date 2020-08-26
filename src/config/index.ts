5053825c3821a5946f121196bed15ad10b77c4ab
codedump00 <blazinasis@gmail.com>
2020-08-26 13:26:01 +0545

    using unsecure smtp channel

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ import { createTransport } from 'nodemailer';
 
 export const transporter = createTransport({
     host: process.env.MAIL__HOST,
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.MAIL__MAIL,
         pass: process.env.MAIL__PASSWORD,
